4cc9f944dc712819f3a5caecf87af4a3383ec6bb
NorskNoobing <39239702+NorskNoobing@users.noreply.github.com>
2022-10-10 15:33:59 +0200

    Update Install-RSAT.ps1

--- source/Public/Install-RSAT.ps1
+++ source/Public/Install-RSAT.ps1
@@ -2,7 +2,6 @@
     Prerequisites: 
     * gsudo
 #>
-#Requires -module NN.WindowsSetup
 function Install-RSAT {
     param (
         [switch]$WUServerBypass,
@@ -13,18 +12,23 @@ function Install-RSAT {
         $null = Get-ADUser -Filter "Name -eq 0"
     }
     catch [System.Management.Automation.CommandNotFoundException] {
-        Invoke-gsudo {
-            Import-Module NN.WindowsSetup
-
-            if ($using:WUServerBypass) {
-                Invoke-WUServerBypass
-            }
-        
-            $RSATNames = (Get-WindowsCapability -Name RSAT* -online | Where-Object State -NotLike 'Installed').Name 
+        try {
+            Invoke-gsudo {
+                Import-Module NN.WindowsSetup
+    
+                if ($using:WUServerBypass) {
+                    Invoke-WUServerBypass
+                }
             
-            foreach ($item in $RSATNames) {
-                Get-WindowsCapability -Name $item -Online | Add-WindowsCapability -Online
+                $RSATNames = (Get-WindowsCapability -Name RSAT* -online | Where-Object State -NotLike 'Installed').Name 
+                
+                foreach ($item in $RSATNames) {
+                    Get-WindowsCapability -Name $item -Online | Add-WindowsCapability -Online
+                }
             }
+        }
+        catch [System.Management.Automation.CommandNotFoundException] {
+            throw "Please run Install-gsudo before running this command."
         }
     }
 }
