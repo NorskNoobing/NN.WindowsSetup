03d4942fae0d42eec86de97352ba315625dc708f
NorskNoobing <39239702+NorskNoobing@users.noreply.github.com>
2022-10-14 15:40:08 +0200

    Update Install-Choco.ps1
    * Use gsudo to automatically elevate

--- source/Public/Install-Choco.ps1
+++ source/Public/Install-Choco.ps1
@@ -3,18 +3,12 @@ function Install-Choco {
         $null = choco --version
     }
     catch [System.Management.Automation.CommandNotFoundException] {
-        if (Test-Path "$env:ProgramData\chocolatey\bin\choco.exe") {
-            Write-Warning "Please restart all terminal applications before using the `"choco`" command."
-        } else {
-            [bool]$runAsAdmin = ([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544"
-            if ($runAsAdmin) {
-                Set-ExecutionPolicy Bypass -Scope Process -Force
-                [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-                Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-                Write-Warning "You'll need to restart all terminal applications before using choco."
-            } else {
-                throw "Function needs to run with administrator privledges."
-            }
+        Invoke-gsudoCheck
+        Invoke-gsudo {
+            Set-ExecutionPolicy Bypass -Scope Process -Force
+            [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+            Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+            Write-Warning "You'll need to restart all terminal applications before using choco."
         }
     }
 }
