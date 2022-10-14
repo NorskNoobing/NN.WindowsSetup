b864ce9620bbde6804ce71333ebd7dd6beab84e6
NorskNoobing <39239702+NorskNoobing@users.noreply.github.com>
2022-10-14 11:52:22 +0200

    Update Install-gsudo.ps1

--- source/Public/Install-gsudo.ps1
+++ source/Public/Install-gsudo.ps1
@@ -4,5 +4,6 @@ function Install-gsudo {
     }
     catch [System.Management.Automation.CommandNotFoundException] {
         Set-ExecutionPolicy RemoteSigned -scope Process -Force; [Net.ServicePointManager]::SecurityProtocol = 'Tls12'; Invoke-WebRequest -useb https://raw.githubusercontent.com/gerardog/gsudo/master/installgsudo.ps1 | Invoke-Expression
+        Write-Warning "You'll need to restart all terminal applications before using gsudo."
     }
 }
