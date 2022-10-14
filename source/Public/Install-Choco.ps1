d0e3b950e4315c0f6e9b6d2bccbd24ce5fd14968
NorskNoobing <39239702+NorskNoobing@users.noreply.github.com>
2022-10-14 15:29:52 +0200

    Update Install-Choco.ps1

--- source/Public/Install-Choco.ps1
+++ source/Public/Install-Choco.ps1
@@ -11,10 +11,10 @@ function Install-Choco {
                 Set-ExecutionPolicy Bypass -Scope Process -Force
                 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
                 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+                Write-Warning "You'll need to restart all terminal applications before using choco."
             } else {
                 throw "Function needs to run with administrator privledges."
             }
         }
     }
 }
-#todo: check if restart is required
