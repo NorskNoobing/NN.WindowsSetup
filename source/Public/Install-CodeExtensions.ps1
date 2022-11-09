b2b35bb6f3db32a01b1fc153501e19bbd963c73f
NorskNoobing <39239702+NorskNoobing@users.noreply.github.com>
2022-11-09 18:04:57 +0100

    Update Install-CodeExtensions.ps1

--- source/Public/Install-CodeExtensions.ps1
+++ source/Public/Install-CodeExtensions.ps1
@@ -1,41 +1,22 @@
 function Install-CodeExtensions {
-    try {
-        code --version
-    }
-    catch [System.Management.Automation.CommandNotFoundException] {
-        throw "Please run `"Install-VSCode`" before running this command."
-    }
-
-    $CodeExtensions = @(
-        "bagetx.inf",
-        "bungcip.better-toml",
-        "eamodio.gitlens",
-        "formulahendry.auto-close-tag",
-        "formulahendry.auto-rename-tag",
-        "GEEKiDoS.vdf",
-        "GitHub.remotehub",
-        "IronGeek.vscode-env",
-        "leighlondon.eml",
-        "ms-azuretools.vscode-docker",
-        "ms-python.python",
-        "ms-python.vscode-pylance",
-        "ms-toolsai.jupyter",
-        "ms-toolsai.jupyter-keymap",
-        "ms-toolsai.jupyter-renderers",
-        "ms-vscode-remote.remote-containers",
-        "ms-vscode-remote.remote-ssh",
-        "ms-vscode-remote.remote-ssh-edit",
-        "ms-vscode.powershell",
-        "PKief.material-icon-theme",
-        "ritwickdey.LiveServer",
-        "royaction.color-manager",
-        "WallabyJs.quokka-vscode",
-        "wayou.vscode-todo-highlight",
-        "xcad2k.vscode-thedigitallife",
-        "icrawl.discord-vscode"
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)][string]$ExtensionListPath
     )
+
+    begin {
+        try {
+            $null = code --version
+        }
+        catch [System.Management.Automation.CommandNotFoundException] {
+            Write-Error "Please run `"Install-VSCode`" before running this command." -ErrorAction Stop
+        }
+    }
     
-    foreach ($item in $CodeExtensions) {
-        code --install-extension $item
+    process {
+        $CodeExtensions = Get-Content -Path $ExtensionListPath
+        $CodeExtensions.ForEach({
+            code --install-extension $_
+        })
     }
 }
