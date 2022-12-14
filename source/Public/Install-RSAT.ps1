017cc9b6cabb16398706256780dc75232ce985d1
NorskNoobing <39239702+NorskNoobing@users.noreply.github.com>
2022-12-14 11:18:15 +0100

    Update Install-RSAT.ps1

--- source/Public/Install-RSAT.ps1
+++ source/Public/Install-RSAT.ps1
@@ -1,34 +1,36 @@
-<#
-    Prerequisites: 
-    * gsudo
-#>
 function Install-RSAT {
+    [CmdletBinding()]
     param (
-        [switch]$WUServerBypass,
-        [string]$ModulePathPassthrough
+        [switch]$WUServerBypass
     )
 
-    try {
-        $null = Get-ADUser -Filter "Name -eq 0"
+    begin {
+        Invoke-gsudoCheck
     }
-    catch [System.Management.Automation.CommandNotFoundException] {
+
+    process {
         try {
-            Invoke-gsudo {
-                Import-Module NN.WindowsSetup
-    
-                if ($using:WUServerBypass) {
-                    Invoke-WUServerBypass
-                }
-            
-                $RSATNames = (Get-WindowsCapability -Name RSAT* -online | Where-Object State -NotLike 'Installed').Name 
-                
-                foreach ($item in $RSATNames) {
-                    Get-WindowsCapability -Name $item -Online | Add-WindowsCapability -Online
-                }
-            }
+            $null = Get-ADUser -Filter "Name -eq 0"
         }
         catch [System.Management.Automation.CommandNotFoundException] {
-            throw "Please run `"Install-gsudo`" before running this command."
+            try {
+                Invoke-gsudo {
+                    Import-Module NN.WindowsSetup
+        
+                    if ($using:WUServerBypass) {
+                        Invoke-WUServerBypass
+                    }
+                
+                    $RSATNames = (Get-WindowsCapability -Name RSAT* -online | Where-Object State -NotLike 'Installed').Name 
+                    
+                    foreach ($item in $RSATNames) {
+                        Get-WindowsCapability -Name $item -Online | Add-WindowsCapability -Online
+                    }
+                }
+            }
+            catch [System.Management.Automation.CommandNotFoundException] {
+                throw "Please run `"Install-gsudo`" before running this command."
+            }
         }
     }
 }
